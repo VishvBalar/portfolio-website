8907af7001c1518deb193110b48a133ea4803008
VishvBalar <147968054+VishvBalar@users.noreply.github.com>
2024-06-07 12:49:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".burger-icone");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
